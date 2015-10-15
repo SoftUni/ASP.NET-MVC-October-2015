e179fc709ce1ee8c01adc2a556bd20a87ba0ba7d
iordan93 <iordan93@gmail.com>
2015-10-15 17:17:54 +0300

    Added an HTML extension

---
+++ SoftUni.LaptopStore/LaptopStore.App/Controllers/LaptopsController.cs
@@ -0,0 +1,20 @@
+﻿namespace LaptopStore.App.Controllers
+{
+    using Data;
+    using System.Web.Mvc;
+
+    public class LaptopsController : Controller
+    {
+        public ActionResult Details(int id)
+        {
+            var context = new ApplicationDbContext();
+            var laptop = context.Laptops.Find(id);
+            if (laptop == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return View(laptop);
+        }
+    }
+}
---
+++ SoftUni.LaptopStore/LaptopStore.App/HtmlHelpers/HtmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace LaptopStore.App.HtmlHelpers
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString Image(
+            this HtmlHelper helper,
+            string imageUrl,
+            string altText = "")
+        {
+            var imgBuilder = new TagBuilder("img");
+            imgBuilder.MergeAttribute("src", imageUrl);
+            imgBuilder.MergeAttribute("alt", altText);
+
+            return new MvcHtmlString(imgBuilder.ToString());
+        }
+    }
+}
